af1ae2571895e0f8c85b0c4d49edb242c6d82643
Aman Raj <amanraj6301@gmail.com>
2026-04-09 21:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 49.7 MB (98.98%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low = 0, high = n-1;
+        int ans =0;
+        while(low <= high){
+            int mid = low +( high - low)/2;
+            if((n - mid) <= citations[mid]){
+                ans = n-mid;
+                high = mid-1;
+                
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
