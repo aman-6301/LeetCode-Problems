f2c77a9390cc72d3751c4371d2f729d42ffd76cb
Aman Raj <amanraj6301@gmail.com>
2026-06-30 10:59:04 +0530

    Time: 6 ms (14.69%) | Memory: 46.6 MB (46.01%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,61 +1,56 @@
-class Solution {
+public class Solution {
     private List<List<String>> result = new ArrayList<>();
 
     public List<List<String>> solveNQueens(int n) {
-        if(n == 0) return result;
+        if (n == 0)
+            return result;
+
         List<String> board = new ArrayList<>();
-        for(int i=0;i<n;i++){
+       
+        for (int i = 0; i < n; i++) {
             StringBuilder row = new StringBuilder();
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 row.append('.');
             }
             board.add(row.toString());
         }
-        solve(board,0);
+
+        int startRow = 0;
+        HashSet<Integer> cols = new HashSet<>();
+        HashSet<Integer> diags = new HashSet<>();
+        HashSet<Integer> antiDiags = new HashSet<>();
+        solve(board, startRow, cols, diags, antiDiags);
+
         return result;
     }
 
-    private void solve(List<String> board, int row){
-
-        if(row == board.size()){
+    private void solve(List<String> board, int row, HashSet<Integer> cols, HashSet<Integer> diagR, HashSet<Integer> diagL) {
+        if (row == board.size()) {
             result.add(new ArrayList<>(board));
             return;
         }
 
-        for(int col = 0;col < board.size();col++){
-            if(isValid(board, row, col)){
-                StringBuilder newRow = new StringBuilder(board.get(row));
-                newRow.setCharAt(col,'Q');
-                board.set(row,newRow.toString());
+        for (int col = 0; col < board.size(); col++) {
+            int diagLId = row - col;
+            int diagRId = row + col;
 
-                solve(board,row+1);
+            if (cols.contains(col) || diagR.contains(diagRId) || diagL.contains(diagLId))
+                continue;
 
-                newRow.setCharAt(col, '.');
-                board.set(row,newRow.toString());
-            }
+            cols.add(col);
+            diagR.add(diagRId);
+            diagL.add(diagLId);
+            StringBuilder newRow = new StringBuilder(board.get(row));
+            newRow.setCharAt(col, 'Q');
+            board.set(row, newRow.toString());
+
+            solve(board, row + 1, cols, diagR, diagL);
+
+            cols.remove(col);
+            diagR.remove(diagRId);
+            diagL.remove(diagLId);
+            newRow.setCharAt(col, '.');
+            board.set(row, newRow.toString());
         }
-    }
-
-    private boolean isValid(List<String>  board, int row, int col){
-        //up
-        for(int i=row-1;i>=0;i--){
-            if(board.get(i).charAt(col) == 'Q'){
-                return false;
-            }
-        }
-
-        //left diagonal upwards
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board.get(i).charAt(j) == 'Q')
-                return false;
-        }
-
-        //right diagonal upwards
-        for (int i = row, j = col; i >= 0 && j < board.size(); i--, j++) {
-            if (board.get(i).charAt(j) == 'Q')
-                return false;
-        }
-
-        return true;
     }
 }
