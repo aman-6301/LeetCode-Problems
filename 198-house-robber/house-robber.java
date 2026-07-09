adbcd680ca206e6adce9d5249960cac0449a8572
Aman Raj <amanraj6301@gmail.com>
2026-07-09 12:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (18.54%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,22 +2,17 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        for(int i=0;i<n;i++){
-            dp[i] = -1;
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i>1){
+                take += dp[i-2];
+            }
+            int notTake = dp[i-1];
+            dp[i] = Math.max(take, notTake);
         }
-        return find(n-1,nums,dp);
-    }
 
-    public int find(int idx, int[] nums, int[] dp){
-        if(idx == 0) return nums[0];
-        if(idx < 0) return 0;
 
-        if(dp[idx] != -1) return dp[idx];
-
-        int take = nums[idx] + find(idx-2,nums,dp);
-
-        int notTake = find(idx-1,nums,dp);
-
-        return dp[idx] = Math.max(take, notTake);
+        return dp[n-1];
     }
 }
