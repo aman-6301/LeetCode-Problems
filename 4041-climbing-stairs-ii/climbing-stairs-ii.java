509baa9c2556384c4de8d3d9951940dd1e6b6328
Aman Raj <amanraj6301@gmail.com>
2026-08-18 21:46:25 +0530

    Time: 5 ms (86.32%) | Memory: 117.3 MB (41.45%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,34 +1,34 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n, costs, dp);
-    }
 
-    public int solve(int n, int[] costs, int[] dp) {
+        int[] dp = new int[n + 1];
 
-        // We are already at the starting stair
-        if (n == 0)
-            return 0;
+        dp[0] = 0;
 
-        // For stair 1, only 0 -> 1 is possible
+        dp[1] = costs[0] + 1;
+
         if (n == 1)
-            return costs[0] + 1;
+            return dp[1];
 
-        // For stair 2, we can come from 0 or 1
-        if (n == 2) {
-            int from0 = costs[1] + 4;
-            int from1 = solve(1, costs,dp) + costs[1] + 1;
+        dp[2] = Math.min(
+            costs[1] + 4,
+            dp[1] + costs[1] + 1
+        );
 
-            return Math.min(from0, from1);
+        for (int i = 3; i <= n; i++) {
+
+            int oneStep = dp[i - 1] + costs[i - 1] + 1;
+
+            int twoStep = dp[i - 2] + costs[i - 1] + 4;
+
+            int threeStep = dp[i - 3] + costs[i - 1] + 9;
+
+            dp[i] = Math.min(
+                oneStep,
+                Math.min(twoStep, threeStep)
+            );
         }
 
-        if(dp[n] != -1) return dp[n];
-
-        int oneStep = solve(n - 1, costs,dp) + costs[n - 1] + 1;
-        int twoStep = solve(n - 2, costs,dp) + costs[n - 1] + 4;
-        int threeStep = solve(n - 3, costs,dp) + costs[n - 1] + 9;
-
-        return dp[n] = Math.min(oneStep, Math.min(twoStep, threeStep));
+        return dp[n];
     }
 }
